111a6c7dd6f4d917f91b7b15ee86c483dfd94cbd
RoshniSingh2068 <roshnisingh1228@gmail.com>
2024-08-26 10:03:50 +0530

    Create user input c++.c

---
+++ user input c++.c
@@ -0,0 +1,12 @@
+# include <iostream>
+
+using namespace std;
+
+int main(){
+    int x;
+    cout <<"type a number"; //type a number and press enter
+    cin>> x; // to get user output
+    cout << "your number is " << x;
+    return 0;
+}
+
